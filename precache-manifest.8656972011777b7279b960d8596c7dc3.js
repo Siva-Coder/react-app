21175e7cb98a6ba48afbbd698a2b7429a8a6bd26
siva-coder <sambasivarao1253@gmail.com>
2020-08-28 20:22:37 +0530

    Updates

---
+++ precache-manifest.8656972011777b7279b960d8596c7dc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1417ffafb3b7491d3da7d89703d9194a",
+    "url": "/Siva-Coder/react-app/index.html"
+  },
+  {
+    "revision": "3fe24513cbb0d8a352c0",
+    "url": "/Siva-Coder/react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0babbde14e333bc689a2",
+    "url": "/Siva-Coder/react-app/static/js/2.8aaac6e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Siva-Coder/react-app/static/js/2.8aaac6e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fe24513cbb0d8a352c0",
+    "url": "/Siva-Coder/react-app/static/js/main.6cb74eef.chunk.js"
+  },
+  {
+    "revision": "fa2900659c8cb69c56a6",
+    "url": "/Siva-Coder/react-app/static/js/runtime-main.89cd132d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Siva-Coder/react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
